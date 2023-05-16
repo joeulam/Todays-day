8bde3a9426126b99778e2da7f1453df4b74f0564
Joey Lam <joeulam345@gmail.com>
2023-05-15 22:29:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("location");
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
