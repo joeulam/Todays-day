8d9ddabc91bfbf4e29a98a1a88d93a53f61a9a33
joeulam <joeulam345@gmail.com>
2023-05-24 15:17:44 -0400

    mini updatess

---
+++ weather/app/page.js
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+      <head>
+        <title>Weather</title>
+      </head>
+
+      
+        <body>
+
+          <div>
+            <h1 id="temperature">Temp</h1>
+            <h2 id="location">Location</h2>
+          </div>
+
+        </body>
+        
+
+        
+      </div>
+     
+
+    
+
+        
+        
+
+    
+    </main>
+  )
+}
