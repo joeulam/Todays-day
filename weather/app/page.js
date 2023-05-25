69221ffdb7b7c9f765f92677425e96b383f199a9
joeulam <joeulam345@gmail.com>
2023-05-25 15:57:59 -0400

    API Still undefined

--- weather/app/page.js
+++ weather/app/page.js
@@ -1,27 +1,23 @@
 import "../public/globals.css";
 
-export default function Home({ props }) {
-  const temperature = props?.Temp;
-
+export default function Home({props}) {
   return (
     <>
       <head>
         <title>Weather</title>
       </head>
         <body>
-          <h1 id="temp">{temperature}</h1>
-          <h2 id="location">Location</h2>
+          <h1 id="temp">{props?.tempfor}</h1>
+          <h2 id="location">Location:{props.City}</h2>
         </body>
     </>
-  );
+  )
 }
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+    return navigator.geolocation.getCurrentPosition(success);
+  } 
 }
 
 function success(pos) {
@@ -34,17 +30,17 @@ function success(pos) {
   return coordinate;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const location = getLocation();
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${location[0]}&longitude=${location[1]}&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph`
-  );
+  const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location[0]}&longitude=${location[1]}&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph`);
   const data = await response.json();
   const hourlyTemperature = data.hourly.temperature_2m;
+
   const d = new Date();
   const hour = d.getHours();
   console.log(hourlyTemperature);
   const finaltemp = Math.round(hourlyTemperature[hour]);
+
   const locationResponse = await fetch(
     `https://us1.locationiq.com/v1/reverse.php?key=pk.495483f503e16834a4ff3b2f5157164e&lat=${location[0]}&lon=${location[1]}&format=json`
   );
@@ -53,7 +49,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      data:{Temp:finaltemp,City:city}
+      tempfor:data.hourly.temperature_2m,
+      City:city
     },
   };
 }
